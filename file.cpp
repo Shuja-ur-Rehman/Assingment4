03c29e6779bc59916b73eb2e41a3dd77d90dcd4d
Jawad Mughal <jawadmughaldev@gmail.com>
2024-02-06 21:28:43 +0500

    defination of the file is created

---
+++ file.cpp
@@ -0,0 +1,48 @@
+#include<istream>
+using namespace std;
+
+class Door{
+private:
+	bool isOpen;
+	bool isClose;
+public:
+ 	Door();
+
+	void openDoor();
+	void closeDoor();
+	void lockDoor()
+	{
+		cout<<"Door is locked\n"<<endl;
+	}
+
+	void openDoor(){
+		isOpen = true;
+	}
+	void closeDoor(){
+		isOpen = false;
+		isClose = true;
+	}
+	void unlockDoor()
+	{
+		cout<<"Door is unlocked...\n"<<endl;
+	}
+	bool isDoorOpen()const;
+	bool isDoorClose()const;
+};
+
+
+int main()
+{
+	 Door myDoor;
+	 myDoor.closeDoor();
+  	 cout << "Is the door open? " << (myDoor.isDoorOpen() ? "Yes" : "No") << endl;
+   	 myDoor.lockDoor();
+    	 myDoor.unlockDoor();
+
+	 cout << "Is the door open? " << (myDoor.isDoorOpen() ? "Yes" : "No") << endl;
+
+         myDoor.openDoor();
+         cout << "Is the door open? " << (myDoor.isDoorOpen() ? "Yes" : "No") << endl;
+
+	return 0;
+}
